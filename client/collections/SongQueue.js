65df40e761ca6292cd8d763194641352fe7d96ef
Elvio Cavalcante <elvio@madfx.com.br>
2015-04-09 00:03:13 -0700

    Passes the following tests: Add, AppView, LibraryEntryView and LibraryView

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,13 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+  	this.on('add', this.enqueue , this); 
+  },
+
+  enqueue: function(){
+
+	console.log(this);
+	this.models[0].play();
   }
 
 });
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -2,8 +2,8 @@
 var AppModel = Backbone.Model.extend({
 
   initialize: function(params){
-    this.set('currentSong', new SongModel());
-    this.set('songQueue', new SongQueue());
+    this.set('currentSong', new SongModel()); // --> model
+    this.set('songQueue', new SongQueue()); // ----> collection
 
     /* Note that 'this' is passed as the third argument. That third argument is
     the context. The 'play' handler will always be bound to that context we pass in.
@@ -11,10 +11,13 @@ var AppModel = Backbone.Model.extend({
     the 'this' we use that's actually in the funciton (this.set('currentSong', song)) would
     end up refering to the window. That's just what happens with all JS events. The handlers end up
     getting called from the window (unless we override it, as we do here). */
-
-
+ 
     params.library.on('play', function(song){
       this.set('currentSong', song);
+    }, this);
+
+    params.library.on('enqueue', function(song){
+      this.get('songQueue').add(song);
     }, this);
   }
 
